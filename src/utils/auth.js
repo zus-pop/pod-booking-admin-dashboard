0c48915dc9804ef4d7d88fd0e72bd158579da765
QHuyyyyy <huy120904@gmail.com>
2024-11-13 23:47:26 +0700

    SPA

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,28 +1,29 @@
 import { toast } from "react-toastify";
 
 let isHandlingSession = false;
+let navigationCallback = null;
+
+export const registerNavigationCallback = (callback) => {
+  navigationCallback = callback;
+};
 
 export const handleSessionExpired = () => {
-  // Nếu đang xử lý session expired rồi thì không làm gì cả
   if (isHandlingSession) {
     return;
   }
 
-  // Đánh dấu đang xử lý
   isHandlingSession = true;
-
-  // Lưu thông tin hiện tại
   localStorage.setItem('lastPath', window.location.pathname);
   localStorage.setItem('lastEmail', localStorage.getItem('userEmail'));
-
-  // Hiển thị thông báo
   toast.warning("Session expired! Please login again.", { autoClose: 3000 });
 
-  // Xóa token và chuyển hướng sau 3 giây
   setTimeout(() => {
     localStorage.removeItem("token");
-    window.location.href = "/";
-    // Reset flag sau khi đã xử lý xong
+    if (navigationCallback) {
+      navigationCallback("/");
+    } else {
+      window.location.href = "/";
+    }
     isHandlingSession = false;
   }, 3000);
 }; 
